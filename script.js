8bc242c96c7554ffd916e93a1d7c632762a0e33d
kingAnoob0 <zaliyevavalide6@gmail.com>
2026-06-29 10:16:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  alert("💎 Game starting soon...");
+}
+
+function showInfo() {
+  document.getElementById("infoBox").classList.toggle("hidden");
+}
+
+function shop() {
+  document.getElementById("shopBox").classList.toggle("hidden");
+}
